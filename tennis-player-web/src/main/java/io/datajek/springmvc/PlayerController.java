adadca3c6ac728a373b03f26cc718104c0ca1a78
codeForGood300 <vardhan2011harsh@gmail.com>
2022-12-27 10:31:10 +0530

    @RequestParam Annotation

--- tennis-player-web/src/main/java/io/datajek/springmvc/PlayerController.java
+++ tennis-player-web/src/main/java/io/datajek/springmvc/PlayerController.java
@@ -6,12 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PlayerController {
 
     @Autowired
-    PlayerService service;
+    PlayerService playerService;
     //method to handle /showPlayerForm
     @RequestMapping("/showPlayerForm")
     public String showForm () {
@@ -20,13 +21,14 @@ public class PlayerController {
 
     //method to handle /processPlayerForm
     @RequestMapping("/processPlayerForm")
-    public String processForm(HttpServletRequest request, Model model) {
-        String pName = request.getParameter("playerName");
-        Player player = service.getPlayerByName(pName);
-        model.addAttribute("name", pName);
+    public String processForm(@RequestParam("playerName") String pName, Model model) {
+        String theName = pName.toUpperCase();
+        Player player=playerService.getPlayerByName(pName);
+        model.addAttribute("name", theName);
         model.addAttribute("country", player.getNationality());
         model.addAttribute("dob", player.getBirthDate());
         model.addAttribute("titles", player.getTitles());
+
         return "player-detail";
     }
 }

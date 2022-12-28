9963e449ab8b2344c5392e1623f3af5d040b40ef
codeForGood300 <vardhan2011harsh@gmail.com>
2022-12-28 12:51:11 +0530

    Custom Validation Annotation

---
+++ tennis-player-web/src/main/java/io/datajek/springmvc/validation/Head2HeadConstraintValidator.java
@@ -0,0 +1,22 @@
+package io.datajek.springmvc.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class Head2HeadConstraintValidator implements ConstraintValidator <HeadToHead, String> {
+
+    //custom business logic to validate the annotation
+
+    @Override
+    public void initialize(HeadToHead headToHeadScore) {
+
+    }
+
+    @Override
+    public boolean isValid(String headToHeadScore, ConstraintValidatorContext context) {
+
+        boolean result = headToHeadScore.matches("[0-9]{1,2}-[0-9]{1,2}");
+        return result;
+    }
+
+}
---
+++ tennis-player-web/src/main/webapp/WEB-INF/views/stats-confirmation.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Head To Head Stats</title>
+</head>
+<body>
+	<h2>Head-to-Head Statistics</h2>
+	<hr>
+
+	<br>
+    <br>
+	<b>${playerStats.player1}</b> &emsp; vs. &emsp;
+	<b>${playerStats.player2}</b>
+
+	<br>
+	<br> Head-to-Head Score :
+	<b>${playerStats.head2head}</b>
+</body>
+</html>
